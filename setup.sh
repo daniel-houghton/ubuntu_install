f0b12abc711705ba1aa5d5cd52db272ddaa984f6
Daniel Houghton <dhoughton@middlebury.edu>
2025-11-22 07:33:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 sudo systemctl disable systemd-networkd-wait-online.service
 sudo apt update && sudo apt upgrade -y
 sudo apt install ubuntu-desktop -y
